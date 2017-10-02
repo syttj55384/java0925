fc8cb0eab8d2875a6f8b7fd9dd29ba393ff9375a
I432 <syttj55384@gmail.com>
2017-10-02 16:46:29 +0800

    score

---
+++ src/score.java
@@ -0,0 +1,12 @@
+public class score {
+    public static void main(String[] args){
+        java.util.Scanner sc =new java .util.Scanner(System.in);
+        double n1=sc.nextDouble();
+        double n2=sc.nextDouble();
+        double n3=sc.nextDouble();
+
+        double sum=n1+n2+n3;
+        double average=sum/3;
+        System.out.println(sum+""+average);
+    }
+}
